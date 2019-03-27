97d724ee1d68d516824308f5d1b9f60bef414e21
DESKTOP-G6OECAN\Jakob <jasninus8@hotmail.dk>
2019-03-27 19:55:21 +0000

    Miro V1

---
+++ MiroSolutionsHackathon/Assets/GoogleARCore/Examples/HelloAR/Scenes/RequestPermissions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestPermissions : MonoBehaviour
+{
+    [SerializeField] private GameObject sceneParentObject;
+
+    private void Start()
+    {
+        StartCoroutine(RequestWebCamPermission());
+    }
+
+    private IEnumerator RequestWebCamPermission()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Debug.Log("webcam found");
+            EnableScene();
+        }
+        else
+        {
+            Debug.Log("webcam not found");
+            Application.Quit();
+        }
+    }
+
+    private void EnableScene()
+    {
+        sceneParentObject.SetActive(true);
+    }
+}
